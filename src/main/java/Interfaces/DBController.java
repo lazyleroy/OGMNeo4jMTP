97e787a7212f95a089746e1b5040df447d5f124c
simon0420 <simon.bec@live.de>
2016-11-14 11:41:39 +0100

    Updated interfaces

--- src/main/java/Interfaces/DBController.java
+++ src/main/java/Interfaces/DBController.java
@@ -17,43 +17,19 @@ public interface DBController {
     void addSpot(Spot spot);
 
     /**
-     * Automatically removes a spot from the db
+     * Updates the spot in the db
      *
      * @param spot :Spot to remove from the db
      */
-    void removeSpot(Spot spot);
-
-
-
+    void updateSpot(Spot spot);
 
     /**
-     * Returns a particular spot with a given ID and center-point
+     * Returns a particular spot with a given ID
      *
      * @param spotID :ID of the Spot
-     * @param point  :GPS location of the Spot
      * @return Spot :searched Spot
      */
-    Spot getSpot(int spotID, GPS_plus point);
-
-    /**
-     * Returns a particular spot with a given ID and center-point
-     *
-     * @param spotID :ID of the Spot
-     * @param latitude :latitude
-     * @param longitude :longitude
-     * @return Spot :searched Spot
-     */
-    Spot getSpot(long spotID, float latitude, float longitude);
-
-    /**
-     * Returns all spots stored in a specific radius
-     *
-     * @param arg1 :latitude-index
-     * @param arg2 :longitude-index
-     * @param radius :radius
-     * @return ArrayList<Spot> :searched Spots
-     */
-    ArrayList<Spot> getSpots(int arg1, int arg2, double radius);
+    Spot getSpot(long spotID);
 
     /**
      *
@@ -62,4 +38,6 @@ public interface DBController {
      * @return Spot: list of searched Spots
      */
     ArrayList<Spot> getSpots(float latitude, float longitude);
+
+    void addGPSPoints(ArrayList<GPS_plus> gpspoints);
 }
